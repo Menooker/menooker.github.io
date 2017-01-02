9ba7afac9e84df723c207e912a51a29be34e63ce
Menooker <myjisgreat@live.cn>
2017-01-02 15:45:30 +0800

    deploy blog

---
+++ _site/deploy.sh
@@ -0,0 +1,15 @@
+git checkout source
+bundle exec /usr/local/bin/jekyll build
+git add .
+git commit -m "update source"
+cp -r _site/ /tmp/
+git checkout gh-pages
+git rm *
+cp -r /tmp/_site/* ./
+git add .
+git commit -m "deploy blog"
+git push origin gh-pages
+git checkout source
+echo "deploy succeed"
+git push origin source
+echo "push source"
